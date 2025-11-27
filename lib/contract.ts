9f0ade5f077e2193f87d6b25a4d6d48e6d5f341c
chandrika1516 <chanduchandrika024@gmail.com>
2025-11-27 16:50:31 +0530

    first commit

---
+++ lib/contract.ts
@@ -0,0 +1,44 @@
+export const contractAddress = "0xF302134fbC6525bD0be4cA2aA040f3A5F9ff8492";
+
+// Export only the ABI array expected by viem/wagmi
+export const contractABI = [
+  {
+    "inputs": [],
+    "name": "get",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "number",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "_number",
+        "type": "uint256"
+      }
+    ],
+    "name": "set",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
+] as const;
